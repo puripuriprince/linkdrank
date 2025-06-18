c8914929fe720c482558a130bc9e5a11e34705c0
Beaudelaire Tsoungui Nzodoumkouo <beaudelaire@tutamail.com>
2025-06-17 20:22:21 -0400

    added splashscreen

--- frontend-ui/src/components/loading-screen/splash-screen.tsx
+++ frontend-ui/src/components/loading-screen/splash-screen.tsx
@@ -1,35 +1,23 @@
 "use client";
 
-import { cn } from "@/lib/utils";
-import { motion } from "framer-motion";
 import { Portal } from "@radix-ui/react-portal";
-import Image from "next/image";
 import { CONFIG } from "@/global-config";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
 export type SplashScreenProps = {
   portal?: boolean;
   className?: string;
 };
 
-export function SplashScreen({ portal = true, className }: SplashScreenProps) {
+export function SplashScreen({ portal = true }: SplashScreenProps) {
   const content = (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      className={cn(
-        "fixed inset-0 flex items-center justify-center z-50",
-        className,
-      )}
-    >
-      <Image
-        src={`${CONFIG.assetsDir}/logo/logo.svg`}
-        alt="Logo"
-        width={128}
-        height={128}
-        className="animate-bounce w-48 h-48"
+    <div className="fixed inset-0 flex items-center justify-center z-50">
+      <DotLottieReact
+        src={`${CONFIG.assetsDir}/logo/logo.json`}
+        loop
+        autoplay
       />
-    </motion.div>
+    </div>
   );
 
   return portal ? <Portal>{content}</Portal> : content;
